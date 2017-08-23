83cfdc348c65fad5d3a3939a42a820f3535d4907
hosein <info@piorra.ir>
2017-08-23 14:36:00 +0430

    Added the files: still methods are not complete

---
+++ dist/groll.js
@@ -0,0 +1,52 @@
+(function() {
+    'use strict';
+    var _version = '1.0.0',
+    errorstart = 'Groll(' + _version + '): ';
+    $.fn.getGrolled = function (k) {
+            
+    };
+    $.fn.grollEvent = function(p) {
+        var el = $(this);
+        if (arguments.length < 1) {
+            throw new Error(errorstart + 'grollEvent method needs at least 1 argument.')
+        } else if ( p && typeof p === 'object' && arguments.length === 1 ){
+            if ( !('offset' in p) || !(typeof p.offset === 'number') ) {
+                throw new Error(errorstart + 'offset is required for grollEvent method (number)')
+            } else {
+                $(window).scroll(function () {
+                    if ($(this).scrollTop() >= p.offset ){
+                        el.removeClass( p.removeClass );
+                        el.addClass( p.addClass );
+                        if ( p.func && typeof p.func === 'function' ) {
+                            p.func()
+                        }
+                    } else {
+                        el.addClass( p.removeClass );
+                        el.removeClass( p.addClass );
+                    }
+                })
+            }
+        }
+    }
+    $.fn.grollTranslate3d = function (depth) {
+        var el = $(this),
+        d = arguments.length === 0 ? 3 : depth;
+        $(window).scroll(function () {
+            var s = $(this).scrollTop();
+            el.css({
+                'transform': 'translate3d(0, ' + s/d + 'px, 0)'
+            })
+        })
+    }
+    $.fn.grollLiveEffect = function (func) {
+        var el = $(this);
+        if (arguments.length !== 1){
+            throw new Error(errorstart + 'grollLiveEffect needs 1 arguments. (function)')
+        }
+        $(window).scroll(function () {
+            var t = $(this);
+            var e = t.scrollTop();
+            func(e)
+        })
+    }
+}());
