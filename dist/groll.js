0ce2138e1e496b4d7854660aa36d7dc94142188c
hkh12 <hrk.x11@gmail.com>
2017-08-25 17:17:26 +0430

    ...

--- dist/groll.js
+++ dist/groll.js
@@ -72,7 +72,9 @@
         };
         var el = $(this);
         $(el).scroll(function () {
-            var wintop = el.scrollTop(), docheight = d.height(), winheight = el.height();
+            var wintop = el.scrollTop(),
+            docheight = d.height(),
+            winheight = el.height();
             var totalScroll = (wintop/(docheight-winheight))*100;
             func(totalScroll);
         })
